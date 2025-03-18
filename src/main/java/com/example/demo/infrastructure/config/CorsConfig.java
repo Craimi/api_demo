8f8f6130558abed0ed4d853e928f17b7caa5473a
Craimi <jesusmedina.jmh@gmail.com>
2025-03-18 14:10:28 -0600

    Cors Config 2

--- src/main/java/com/example/demo/infrastructure/config/CorsConfig.java
+++ src/main/java/com/example/demo/infrastructure/config/CorsConfig.java
@@ -1,15 +1,23 @@
 package com.example.demo.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://apidemo-production.up.railway.app/alumno")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+public class CorsConfig{
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://apidemo-production.up.railway.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
