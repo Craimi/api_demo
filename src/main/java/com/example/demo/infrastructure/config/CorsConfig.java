f34949e54ab58fd61050a0272c67b01f35a64e87
Craimi <jesusmedina.jmh@gmail.com>
2025-03-18 14:24:10 -0600

    Cors

--- src/main/java/com/example/demo/infrastructure/config/CorsConfig.java
+++ src/main/java/com/example/demo/infrastructure/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://apidemo-production.up.railway.app")
+                .allowedOrigins("https://apidemo-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
